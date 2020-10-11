5e2f39f53f16f91ae3d2b630c839e722e19bf811
vakhno <vakhnohere@gmail.com>
2020-10-12 00:11:12 +0300

    Added fetch requests

--- src/App.js
+++ src/App.js
@@ -10,62 +10,145 @@ import Places from './Places/Places';
 import Currency from './Currency/Currency';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currencyRate: {},
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			currencyRate: {},
+			cashMachines: {},
+			terminals: {},
+			cities: [],
+		}
+	}
 
-  componentDidMount() {
-    this.getRate();
-  }
+	componentDidMount() {
+		this.getRate();
 
-  getRate = () => {
-    fetch(`https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5`, {
-      method: 'GET',
-      mode: 'cors',
-    })
-      .then(data => {
-        return data.json();
-      })
-      .then(data => {
-        let allCurrency = {};
-        for (let curr in data) {
-          allCurrency[data[curr].ccy] = [+data[curr].buy, +data[curr].sale];
-        }
+		this.getCashMachines();
+		this.getSelfServiceTerminals();
+	}
 
-        this.setState({
-          currencyRate: allCurrency,
-        })
-      });
-  }
+	getRate = () => {
+		fetch(`https://cors-anywhere.herokuapp.com/https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5`, {
+			method: 'GET',
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+				'Access-Control-Allow-Origin': '*',
+			},
+			mode: 'cors',
+		})
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				console.log(data)
+				let allCurrency = {};
+				for (let curr in data) {
+					allCurrency[data[curr].ccy] = [+data[curr].buy, +data[curr].sale];
+				}
 
-  render() {
-    return (
-      <Router>
-        <div className="card" >
-          <Header />
-          <Switch>
-            <Route
-              path='/' exact
-              render={props => <Main currencyRate={this.state.currencyRate} />}
-            />
-            <Route path='/places' component={Places} />
-            <Route
-              path='/currency'
-              render={props => <Currency currencyRate={this.state.currencyRate} />}
-            />
-            <Route
-              path='/calculator'
-              render={props => <Calculator currencyRate={this.state.currencyRate} />}
-            />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+				this.setState({
+					currencyRate: allCurrency,
+				})
+				console.log(this.state.currencyRate)
+			});
+	}
+
+	getCashMachines = (city) => {
+		fetch(`https://cors-anywhere.herokuapp.com/https://api.privatbank.ua/p24api/infrastructure?json&atm&address=&city=${city}`, {
+			method: 'GET',
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+				'Access-Control-Allow-Origin': '*',
+			},
+			mode: 'cors',
+		})
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				return data;
+			});
+	}
+
+	getCashMachines = (city) => {
+		fetch(`https://cors-anywhere.herokuapp.com/https://api.privatbank.ua/p24api/infrastructure?json&tso&address=&city=${city}`, {
+			method: 'GET',
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+				'Access-Control-Allow-Origin': '*',
+			},
+			mode: 'cors',
+		})
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				return data;
+			});
+	}
+
+	getSelfServiceTerminals = (e) => {
+		fetch(`https://cors-anywhere.herokuapp.com/https://api.privatbank.ua/p24api/infrastructure?json&atm&address=&city=`, {
+			method: 'GET',
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+				'Access-Control-Allow-Origin': '*',
+			},
+			mode: 'cors',
+		})
+			.then(data => {
+				return data.json();
+			})
+			.then(data => {
+				let allCities = new Set();
+
+				data.devices.forEach((elem, index) => {
+					allCities.add(elem.cityRU)
+				});
+
+				allCities = Array.from(allCities).sort((a, b) => {
+					const firstCity = a.trim();
+					const secondCity = b.trim();
+
+					if (firstCity < secondCity) {
+						return -1;
+					}
+					if (firstCity > secondCity) {
+						return 1;
+					}
+					return 0;
+				})
+				this.setState({
+					cities: allCities,
+				})
+			});
+	}
+
+	render() {
+		return (
+			<Router>
+				<div className="card" >
+					<Header />
+					<Switch>
+						<Route
+							path='/' exact
+							render={props => <Main currencyRate={this.state.currencyRate} />}
+						/>
+						<Route path='/places' component={Places} />
+						<Route
+							path='/currency'
+							render={props => <Currency currencyRate={this.state.currencyRate} />}
+						/>
+						<Route
+							path='/calculator'
+							render={props => <Calculator currencyRate={this.state.currencyRate} />}
+						/>
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
