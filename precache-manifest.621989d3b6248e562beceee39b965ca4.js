907c5d09deccd28d73d95268b3490486461af0ce
vakhno <vakhnohere@gmail.com>
2020-10-17 00:05:28 +0300

    Updates

---
+++ precache-manifest.621989d3b6248e562beceee39b965ca4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc44bf4d5ba04c0948712214693da35",
+    "url": "/PrivateBank-web-card/index.html"
+  },
+  {
+    "revision": "2a7f000585e52e215467",
+    "url": "/PrivateBank-web-card/static/css/main.fcd6b82f.chunk.css"
+  },
+  {
+    "revision": "e4225f46de4b64749e0b",
+    "url": "/PrivateBank-web-card/static/js/2.49244682.chunk.js"
+  },
+  {
+    "revision": "34db1ce62c74dd902339d5a1b99e75b8",
+    "url": "/PrivateBank-web-card/static/js/2.49244682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7f000585e52e215467",
+    "url": "/PrivateBank-web-card/static/js/main.2fcb09a5.chunk.js"
+  },
+  {
+    "revision": "a68ac75d4f1dcc51a6ca",
+    "url": "/PrivateBank-web-card/static/js/runtime-main.06392fa9.js"
+  },
+  {
+    "revision": "0a3869c1cd8e43c813ced5638eb3e9f9",
+    "url": "/PrivateBank-web-card/static/media/Arimo-Bold.0a3869c1.woff"
+  },
+  {
+    "revision": "ee48f8ef3aea935a024b2599b6163dff",
+    "url": "/PrivateBank-web-card/static/media/Arimo.ee48f8ef.woff"
+  },
+  {
+    "revision": "f4639873d6c17cd494e2c89ab2740d22",
+    "url": "/PrivateBank-web-card/static/media/Oswald-Bold.f4639873.woff"
+  },
+  {
+    "revision": "a1652d1ab7a7c988ce7b60c20a531fb6",
+    "url": "/PrivateBank-web-card/static/media/Oswald-Medium.a1652d1a.woff"
+  },
+  {
+    "revision": "ae4c180c568848471dd8ab537030b0c0",
+    "url": "/PrivateBank-web-card/static/media/Oswald-Regular.ae4c180c.woff"
+  },
+  {
+    "revision": "514fe4e255059267db0a128a127f2847",
+    "url": "/PrivateBank-web-card/static/media/Oswald-SemiBold.514fe4e2.woff"
+  },
+  {
+    "revision": "af01b5037ff63cf05210745f4c248269",
+    "url": "/PrivateBank-web-card/static/media/RobotoBold.af01b503.woff"
+  },
+  {
+    "revision": "5ca830617cdc06dbc5e2d46878bba8b1",
+    "url": "/PrivateBank-web-card/static/media/RobotoMedium.5ca83061.woff"
+  },
+  {
+    "revision": "18b2429ba6e7179daeec5438639ab65f",
+    "url": "/PrivateBank-web-card/static/media/RobotoRegular.18b2429b.woff"
+  },
+  {
+    "revision": "923fedab6b2330852748b1d8175f4584",
+    "url": "/PrivateBank-web-card/static/media/arrows.923fedab.svg"
+  },
+  {
+    "revision": "516338f81bb19145db3e0d88adfb44da",
+    "url": "/PrivateBank-web-card/static/media/github.516338f8.svg"
+  },
+  {
+    "revision": "6c1c547f9223ef36b06166ebd53d69d0",
+    "url": "/PrivateBank-web-card/static/media/iphone.6c1c547f.png"
+  },
+  {
+    "revision": "fdeafdce28e1e41af2968ccc99a14ca2",
+    "url": "/PrivateBank-web-card/static/media/logo.fdeafdce.png"
+  },
+  {
+    "revision": "3ec6b887ef58679cfcace8abaf670f5c",
+    "url": "/PrivateBank-web-card/static/media/museum.3ec6b887.svg"
+  },
+  {
+    "revision": "249874c0c3f41cd380d7fa37f1ef8443",
+    "url": "/PrivateBank-web-card/static/media/online-banking.249874c0.svg"
+  },
+  {
+    "revision": "56bc37eaf259398a1d57c5be4ffa6b1e",
+    "url": "/PrivateBank-web-card/static/media/profit.56bc37ea.svg"
+  }
+]);
